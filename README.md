f2b5121086e01a6fdf39888257a13d2865e828f0
Franck Ernewein <franck.ernewein@gmail.com>
2016-10-11 11:59:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tweetping repost
+
+Repost post received from Tweetping platform
+
+## Usage
+
+```
+npm install tweetping-repost --save
+tweetping-repost -u http://localhost:3000/service -s 8596f6fa
+```
+
+### Arguments
+
+```
+-u   URL where post will forwarded
+-s   Stream ID
+-m   HTTP method, GET,POST,PUT,DELETE... 
+```
+
---
+++ index.js
@@ -0,0 +1,20 @@
+const createConnection = require('tweetping-connect').default;
+const parseArgs = require('minimist');
+const fetch =  require('isomorphic-fetch');
+
+const args = parseArgs(process.argv.slice(2));
+
+const streamId = args.s;
+const url = args.u;
+const method = args.m || 'POST';
+
+console.log(`stream: ${streamId}, repost to: ${url}`);
+
+const {connect} = createConnection(streamId);
+
+connect('raw', (post) => {
+  fetch(url, {
+    method,
+    body: JSON.stringify(post)
+  }).then(r => r.json()).then(r => console.log(r));
+});
